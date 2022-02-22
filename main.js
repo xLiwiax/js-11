201b2101e57aa7f72607ebd5392c41d3eb133776
xLiwiax <boliwia333@gmail.com>
2022-02-23 00:36:17 +0100

    zadanie11

---
+++ main.js
@@ -0,0 +1,7 @@
+let nazwa = document.getElementById("nazwa-id");
+let przycisk = document.querySelector('button');
+function fu () {    
+    let tekst = document.querySelector('p');
+    tekst.textContent="troche tekstu";
+}
+przycisk.onclick = fun()
